d80bee15015ef04d335c373130a906f9e1030a93
MithilajHassan <mithilajmt7@gmail.com>
2024-07-06 16:52:08 +0530

    trying connecting

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3'
-export const API_KEY = ''
+export const API_KEY = '0fbb4bb1200c577d3974ac804da667a0'
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
